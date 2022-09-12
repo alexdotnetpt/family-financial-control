247c1703491b46f005aeeb4ddd189bdce7ab0537
alexdotnetpt <alexdotnetpt@gmail.com>
2022-09-12 17:30:48 +0100

    feature: create swagger ui

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import express from 'express'
 const cors = require('cors');
 import { AppDataSource } from './data-source'
-
+const swaggerUi = require('swagger-ui-express'),
+    swaggerDocument = require('../swagger.json');
 AppDataSource.initialize().then(() => {
     const app = express()
 
     app.use(express.json())
-    //app.use(cors())
+    app.use(
+        '/api-docs',
+        swaggerUi.serve, 
+        swaggerUi.setup(swaggerDocument)
+      )
 
     app.get('/', cors(), (req, res) => {
         return res.json({msg: 'This API is working well...!'})
